1cd943268ee7f81fbb3dabb7ead83b41870a8f4d
Manish4586 <Manish.n.manish45@gmail.com>
2018-08-21 20:40:04 +0530

    Update

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,7 +31,7 @@ ENABLE_SCHEDBOOST := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.fastboot=1
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
@@ -41,38 +41,37 @@ TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/Image.gz-dtb
 # Partitions
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 1067958957
 BOARD_FLASH_BLOCK_SIZE := 131072  # (BOARD_KERNEL_PAGESIZE * 64)
-
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 BOARD_ROOT_EXTRA_FOLDERS := bt_firmware dsp firmware persist
 BOARD_USES_RECOVERY_AS_BOOT := true
 BOARD_HAS_REMOVABLE_STORAGE := true
-
 BOARD_SUPPRESS_SECURE_ERASE := true
-
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
 
 # Recovery
 TARGET_RECOVERY_WIPE := $(LOCAL_PATH)/recovery/root/etc/recovery.wipe
 
+# Board Properties
 BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_HAS_NO_SELECT_BUTTON := true
 
-# Vendor
-TARGET_COPY_OUT_VENDOR := vendor
-
 # TWRP Configuration
 TW_THEME := portrait_hdpi
+BOARD_HAS_NO_REAL_SDCARD := true
 TW_INCLUDE_CRYPTO := true
-TW_MAX_BRIGHTNESS := 200
+TW_MAX_BRIGHTNESS := 100
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_EXTRA_LANGUAGES := true
 TW_NO_SCREEN_BLANK := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-RECOVERY_SDCARD_ON_DATA := false
+TW_INCLUDE_NTFS_3G := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TARGET_RECOVERY_DEVICE_MODULES += android.hardware.boot@1.0
 TW_RECOVERY_ADDITIONAL_RELINK_FILES := ${OUT}/system/lib64/android.hardware.boot@1.0.so

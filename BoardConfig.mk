b8eefb99568719ed6736e4ba15a7ea6e2b641790
Manish4586 <Manish.n.manish45@gmail.com>
2018-08-29 15:42:14 +0530

    Update flags for a/b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,15 +76,15 @@ TW_EXCLUDE_DEFAULT_USB_INIT := true
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TARGET_RECOVERY_DEVICE_MODULES += android.hardware.boot@1.0
 TW_RECOVERY_ADDITIONAL_RELINK_FILES := ${OUT}/system/lib64/android.hardware.boot@1.0.so
-#TW_SCREEN_BLANK_ON_BOOT := true
+TW_SCREEN_BLANK_ON_BOOT := true
 
 # Security Patch Hack to prevent Anti Rollback
 PLATFORM_SECURITY_PATCH := 2025-12-31
 
 # A/B partition device flags
 #TARGET_NO_KERNEL := false
-#TARGET_NO_RECOVERY := true
-#BOARD_USES_RECOVERY_AS_BOOT := true
+TARGET_NO_RECOVERY := true
+BOARD_USES_RECOVERY_AS_BOOT := true
 AB_OTA_UPDATER := true
 
 # Official

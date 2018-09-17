984391fa3c145ceca7d12305cf74ae75c800f33f
Manish4586 <Manish.n.manish45@gmail.com>
2018-09-17 16:41:11 +0530

    KERNEL_CMDLINE
    Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,7 +33,7 @@ ENABLE_SCHEDBOOST := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3
-#BOARD_KERNEL_CMDLINE += androidboot.fastboot=1
+BOARD_KERNEL_CMDLINE += androidboot.fastboot=1
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100

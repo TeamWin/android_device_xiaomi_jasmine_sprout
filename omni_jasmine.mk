b1469fea446bf61c7497d31509b76be6c3724d4d
Manish4586 <manish.n.manish45@gmail.com>
2018-11-25 23:18:37 +0530

    jasmine
    	modified:   omni_jasmine.mk
    	modified:   prebuilt/Image.gz-dtb
    	deleted:    recovery/root/vendor/lib64/libicuuc.so
    	deleted:    recovery/root/vendor/lib64/libxml2.so
    	recovery/root/sbin/libicuuc.so
    	recovery/root/sbin/libxml2.so
    
    	modified:   omni_jasmine.mk
    	modified:   prebuilt/Image.gz-dtb
    	deleted:    recovery/root/vendor/lib64/libicuuc.so
    	deleted:    recovery/root/vendor/lib64/libxml2.so
    	recovery/root/sbin/libicuuc.so
    	recovery/root/sbin/libxml2.so

--- omni_jasmine.mk
+++ omni_jasmine.mk
@@ -15,7 +15,7 @@
 #
 
 # Release name
-PRODUCT_RELEASE_NAME := jasmine
+PRODUCT_RELEASE_NAME := jasmine_sprout
 
 $(call inherit-product, build/target/product/embedded.mk)
 
@@ -40,7 +40,7 @@ endif
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := omni_jasmine
-PRODUCT_DEVICE := jasmine
+PRODUCT_DEVICE := jasmine_sprout
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A2
 PRODUCT_MANUFACTURER := Xiaomi
